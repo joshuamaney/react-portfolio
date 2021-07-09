87a0fa4c9cccbb26226a361d7065550f771af263
Joshua Maney <joshuaraymaney@gmail.com>
2021-07-09 19:44:22 -0400

    adding dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-"
+// import { Link } from "react-router-dom";
+// import "./style.css";
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <form class="container-fluid justify-content-start">
+                <button class="btn btn-outline-success me-2" type="button">Home</button>
+                <button class="btn btn-sm btn-outline-secondary" type="button">About</button>
+                <button class="btn btn-sm btn-outline-secondary" type="button">Projects</button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar;
